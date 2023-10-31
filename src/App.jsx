04a82e039c2aad29313a047a83b74030fa08ac6f
akkiS18 <llus77385@gmail.com>
2023-10-31 23:03:55 +0500

    mainbuttonclicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/card/card'
 import Cart from './components/cart/cart'
@@ -43,11 +43,17 @@ const App = () => {
   const onCheckout = () => {
     telegram.MainButton.text = 'Sotib olish :)';
     telegram.MainButton.show();
-
-    if(cartItems.length === 0) {
-      telegram.MainButton.remove()
-    }
   };
+
+  const onSendData = useCallback(() => {
+    telegram.sendData(JSON.stringify(cartItems))
+  }, [cartItems])
+
+  useEffect(() => {
+    telegram.onEvent('mainButtonClicked', onSendData)
+
+    return () => telegram.offEvent('mainButtonClicked', onSendData)
+  }, [onSendData])
 
   return (
     <>
