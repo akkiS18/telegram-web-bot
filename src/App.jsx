27c04ed04322a7c02b5f584ba1a122fcf7ec5f7a
akkiS18 <llus77385@gmail.com>
2023-11-01 18:14:21 +0500

    p

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
         headers: {
           'Content Type': 'application/json',
         },
-        body: JSON.stringify(cartItems),
+        body: JSON.stringify({products: cartItems, queryId: queryId}),
       })
     } else {
       telegram.sendData(JSON.stringify(cartItems))
