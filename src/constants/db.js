0ea4a356e455c2d48960b7c8819c9ae6abd2b357
akkiS18 <llus77385@gmail.com>
2024-02-06 11:28:31 +0500

    add

--- src/constants/db.js
+++ src/constants/db.js
@@ -1,14 +1,14 @@
 export function getData() {
   return [
     {
-      title: "Сороконожка, Futbol oyoq kiyimi , rangi - oq, razmerlar: 39-44",
+      title: "Сороконожка, razmerlar: 39-44",
       price: 190,
       Image:
         "https://cf-vectorizer-live.s3.amazonaws.com/uploads/anon/2boeZkvAkBelgUUFODBjd81IWUr.jpeg",
       id: 1,
     },
     {
-      title: "Сороконожка, Futbol oyoq kiyimi , rangi - qora, razmerlar: 39-44",
+      title: "Сороконожка, razmerlar: 39-44",
       price: 190,
       Image:
         "https://cf-vectorizer-live.s3.amazonaws.com/uploads/anon/2bofIqODe2A6ZeIxnaJwUiXC4ok.jpeg",
